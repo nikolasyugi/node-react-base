f4389a80d91a4a8fc493f6086fee4c9fe15f68fe
Nikolas Athanasopoulos <nikolas.yugi@gmail.com>
2021-03-06 17:00:07 -0300

    send specific error messages to specific http codes

--- api/lib/errorHandling.js
+++ api/lib/errorHandling.js
@@ -16,8 +16,13 @@ module.exports = () => {
                     break;
             }
             res.status(err.status || 500);
-            if (process.env.NODE_ENV === "development") return res.json({ message: msgCustomized || err.message });
-            else return res.json({ message: "Internal server error" });
+            if (process.env.NODE_ENV !== "production") return res.json({ message: msgCustomized || err.message });
+            else {
+                if (err.status === 500) return res.json({ message: "Internal server error" });
+                else if (err.status === 400) return res.json({ message: "Bad request" });
+                else if (err.status === 404) return res.json({ message: "Not found" });
+                else return res.json({ message: `Error ${err.status}` });
+            }
         },
 
         routeNotFound: (next) => {
