ec1fd792680d5eacbe137f22a204c60c3e997809
Nikolas Athanasopoulos <nikolas.yugi@gmail.com>
2021-03-15 00:48:09 -0300

    adjusted password hashing

--- api/models/User.js
+++ api/models/User.js
@@ -25,17 +25,23 @@ const UserSchema = new mongoose.Schema({
 
 // on every save hash password
 UserSchema.pre('save', function (next) {
-    //hash password
-    if (this.password != undefined) {
-        return bcrypt.hash(this.password, 10)
-            .then(hash => {
-                this.password = hash;
-            })
-            .catch(err => {
-                throw new Error();
-            });
-    }
-    next();
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
 });
 
 UserSchema.methods.verifyPassword = function (passwordSent) {
